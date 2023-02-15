39d0125d2b9f353580c5fbaa49a36044863d6837
AbiGino <115475078+AbiGino@users.noreply.github.com>
2023-02-15 12:31:12 +0530

    adding files

---
+++ src/Mypack/ProtectedDemo1.java
@@ -0,0 +1,10 @@
+package Mypack;
+import pack.*;
+
+ class ProtectedDemo1 extends ProtectedDemo {
+     public static void main(String args[]){
+         ProtectedDemo1 obj = new ProtectedDemo1();
+         obj.msg();
+     }
+
+ }
---
+++ src/pack/ProtectedDemo.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class ProtectedDemo {
+    protected void msg(){
+        System.out.println("Hello");
+    }
+}
