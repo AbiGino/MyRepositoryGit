05ff7998d682aaa49a35c041572959fb6f0331f7
AbiGino <115475078+AbiGino@users.noreply.github.com>
2023-02-15 15:58:08 +0530

    adding files

---
+++ src/LambdaExpressionTwo.java
@@ -0,0 +1,15 @@
+interface Addable{
+    int add(int a,int b);
+}
+
+public class LambdaExpressionTwo {
+    public static void main(String[] args){
+
+        Addable a1=(a,b)->(a+b);
+        System.out.println(a1.add(10,20));
+
+
+        Addable a2=(a,b)->(a+b);
+        System.out.println(a2.add(100,200));
+    }
+}
